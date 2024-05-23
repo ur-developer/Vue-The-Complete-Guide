bd005e2114b7a216d7064c424c3a0d72f0201836
Paul Park <alsrl9295@naver.com>
2024-05-23 10:12:49 +0900

    First Commit

---
+++ TheMonsterSlayerGame/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth : 100,
+            monsterHealth : 100
+        };
+    },
+
+    methods: {
+        attackMonster() {
+           const attackValue = Math.floor(Math.random() * (12 - 5)) + 5;
+           this.monsterHealth -= attackValue;
+        }
+    }
+})
+
+app.moit('#game');
