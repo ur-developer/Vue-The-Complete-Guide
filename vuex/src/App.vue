cf38fa54c5c5ceb909dee230d4ff76b2ce365235
Paul Park <ur_developer92@naver.com>
2024-05-30 16:14:04 +0900

    new-vue-project <Creating a Basic Vue App> , <props , componets>

---
+++ vuex/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <base-container title="Vuex">
+    <TheCounter></TheCounter>
+    <button @click="addOne">Add 1</button>
+    <ChangeCount></ChangeCount>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue'
+import TheCounter from './components/TheCounter.vue'
+import ChangeCount from './components/ChangeCount.vue'
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCount,    
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
