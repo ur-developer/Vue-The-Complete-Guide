99742a5c4cc5c2746066073d9cc71a4d0a3f7c1f
Paul Park <ur_developer92@naver.com>
2024-06-12 21:45:12 +0900

    Diving-Deeper-Into-Components commit

---
+++ Diving-Deeper-Into-Components/src/components/BadgeList.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <base-card>
+      <template #default>
+        <ul>
+          <li>
+            <base-badge type="admin" caption="ADMIN"></base-badge>
+          </li>
+          <li>
+            <base-badge type="author" caption="AUTHOR"></base-badge>
+          </li>
+        </ul>
+      </template>
+    </base-card>
+  </section>
+</template>
+
+<style>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+}
+
+li {
+  margin-right: 1rem;
+}
+</style>
